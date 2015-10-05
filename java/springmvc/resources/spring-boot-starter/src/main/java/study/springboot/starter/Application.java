0909bd4d857f01652db69422906647f45ebdb18c
jiangnan <jgnan1981@163.com>
2015-10-05 18:05:50 +0800

    Complete the example project

--- java/springmvc/resources/spring-boot-starter/src/main/java/study/springboot/starter/Application.java
+++ java/springmvc/resources/spring-boot-starter/src/main/java/study/springboot/starter/Application.java
@@ -2,10 +2,10 @@ package study.springboot.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@ComponentScan(basePackages="study.springboot.starter")
+@SpringBootApplication
 @EnableAutoConfiguration
 @ImportResource("classpath:/application-context.xml")
 public class Application {
