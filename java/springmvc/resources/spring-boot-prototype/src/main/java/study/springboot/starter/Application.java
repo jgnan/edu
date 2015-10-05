f0175c58fd12126ccd1e47805a27f4cbc370e4cc
江南 <jgnan1981@163.com>
2015-10-05 17:38:56 +0800

    fix project

--- java/springmvc/resources/spring-boot-prototype/src/main/java/study/springboot/starter/Application.java
+++ java/springmvc/resources/spring-boot-prototype/src/main/java/study/springboot/starter/Application.java
@@ -1,11 +1,11 @@
-package study.springmvc;
+package study.springboot.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
-@ComponentScan(basePackages="com.imooc.springmvc")
+@ComponentScan(basePackages="study.springboot.starter")
 @EnableAutoConfiguration
 @ImportResource("classpath:/application-context.xml")
 public class Application {
