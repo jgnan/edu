62e7ab9bb9d48bcff6d1b4429624a5d34d67ecb7
jiangnan <jgnan1981@163.com>
2015-10-06 08:42:43 +0800

    remove useless annotations

--- java/springmvc/resources/spring-boot-starter/src/main/java/study/springboot/starter/Application.java
+++ java/springmvc/resources/spring-boot-starter/src/main/java/study/springboot/starter/Application.java
@@ -1,12 +1,10 @@
 package study.springboot.starter;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ImportResource("classpath:/application-context.xml")
 public class Application {
 
