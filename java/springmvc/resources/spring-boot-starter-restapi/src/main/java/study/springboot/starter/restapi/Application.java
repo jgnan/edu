50c3f2a3f91b960bb91c661e3b3a1a2da8d6b287
江南 <jgnan1981@163.com>
2015-10-08 08:00:23 +0800

    divide prototype into restapi and web type

---
+++ java/springmvc/resources/spring-boot-starter-restapi/src/main/java/study/springboot/starter/restapi/Application.java
@@ -0,0 +1,15 @@
+package study.springboot.starter.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:/application-context.xml")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
--- java/springmvc/resources/spring-boot-starter-web/src/main/java/study/springboot/starter/web/Application.java
+++ java/springmvc/resources/spring-boot-starter-web/src/main/java/study/springboot/starter/web/Application.java
@@ -1,7 +1,9 @@
-package study.springboot.example;
+package study.springboot.starter.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
