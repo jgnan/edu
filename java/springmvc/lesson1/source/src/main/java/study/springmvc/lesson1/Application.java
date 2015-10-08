be6fe1b4ca7f6eebb493c8d1f66bfeae0a545286
江南 <jgnan1981@163.com>
2015-10-08 08:01:29 +0800

    update code

--- java/springmvc/lesson1/source/src/main/java/study/springmvc/lesson1/Application.java
+++ java/springmvc/lesson1/source/src/main/java/study/springmvc/lesson1/Application.java
@@ -2,8 +2,6 @@ package study.springmvc.lesson1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
