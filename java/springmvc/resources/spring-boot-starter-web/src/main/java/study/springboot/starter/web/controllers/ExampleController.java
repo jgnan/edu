1d759f5e032b04c9ef7e7c8caccbbabb30a90e0a
江南 <jgnan1981@163.com>
2015-10-20 16:34:20 +0800

    add example

---
+++ java/springmvc/resources/spring-boot-starter-web/src/main/java/study/springboot/starter/web/controllers/ExampleController.java
@@ -0,0 +1,49 @@
+package study.springboot.starter.web.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/examples")
+public class ExampleController {
+	@RequestMapping("/")
+	public String list(Map<String, Object> model){
+		return "/examples/index";
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public String view(Map<String, Object> model,@PathVariable("id")Long id){
+		return "/examples/view";
+	}
+	
+	@RequestMapping(value="/new",method=RequestMethod.GET)
+	public String newForm(Map<String, Object> model){
+		return "/examples/new";
+	}
+	
+	@RequestMapping(value="/",method=RequestMethod.POST)
+	public String create(Map<String, Object> model){
+		long id = 0;
+		return "redirect:/examples/"+id;
+	}
+	
+	@RequestMapping(value="/{id}/edit",method=RequestMethod.GET)
+	public String editForm(Map<String, Object> model,@PathVariable("id")String id){
+		return "/examples/edit";
+	}
+	
+	@RequestMapping(value="/{id}/",method=RequestMethod.POST)
+	public String update(Map<String, Object> model){
+		long id = 0l;
+		return "redirect:/examples"+id;
+	}
+	
+	@RequestMapping(value="/{id}/",method=RequestMethod.DELETE)
+	public String delete(Map<String, Object> model){
+		return "redirect:/examples/";
+	}
+}
