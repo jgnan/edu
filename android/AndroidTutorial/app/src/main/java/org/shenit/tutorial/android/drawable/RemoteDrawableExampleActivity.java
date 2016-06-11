c9a04ce4bd12b5f7a6df2fafbd3f405a924b785d
江南 <jgnan1981@163.com>
2016-06-11 22:53:57 +0800

    Fix some problem in RemoteDrawable example

--- android/AndroidTutorial/app/src/main/java/org/shenit/tutorial/android/drawable/RemoteDrawableExampleActivity.java
+++ android/AndroidTutorial/app/src/main/java/org/shenit/tutorial/android/drawable/RemoteDrawableExampleActivity.java
@@ -5,24 +5,24 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatImageView;
+import android.widget.ImageView;
 
+import org.apache.commons.io.IOUtils;
 import org.shenit.tutorial.android.R;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class RemoteDrawableExampleActivity extends AppCompatActivity {
-    AppCompatImageView img1,img2;
+    ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_drawable_example);
         //load by async task directly.
-        img1 = (AppCompatImageView) findViewById(R.id.img1);
+        img1 = (ImageView) findViewById(R.id.img1);
         new LoadDrawableTask().execute();
 
     }
@@ -31,14 +31,15 @@ public class RemoteDrawableExampleActivity extends AppCompatActivity {
     class LoadDrawableTask extends AsyncTask<Void, Void, Bitmap>{
         @Override
         protected Bitmap doInBackground(Void... params) {
+            InputStream is = null;
             try {
                 URL url = new URL("http://www.wallpapersxl.com/wallpapers/1366x768/planes/102179/planes-crazy-rabbit-vote-hitchhiking-group-102179.jpg");
-                InputStream is = url.openStream();
+                is = url.openStream();
                 return  BitmapFactory.decodeStream(is);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            }finally{
+                IOUtils.closeQuietly(is);
             }
             return null;
         }
