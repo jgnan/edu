677ae224cad3f7abe9fce67999260b3a5433329c
江南 <jgnan1981@163.com>
2016-06-23 23:11:13 +0800

    更新PullToRefresh的代码

--- android/AndroidTutorial/app/src/main/java/org/shenit/tutorial/android/list/PullToRefreshExampleActivity.java
+++ android/AndroidTutorial/app/src/main/java/org/shenit/tutorial/android/list/PullToRefreshExampleActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ListView;
 
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
@@ -19,47 +18,66 @@ public class PullToRefreshExampleActivity extends AppCompatActivity {
     private int page = 1;
     private Handler handler = new Handler();
     private PullToRefreshListView list;
+    private ArticleListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pull_to_refresh_example);
         list = (PullToRefreshListView) findViewById(R.id.list_view);
-        final ArticleListAdapter adapter = new ArticleListAdapter(this, Article.find(Article.class, null, null, null, null, "10"));
+        adapter = new ArticleListAdapter(this, Article.find(Article.class, null, null, null, null, "10"));
         list.setAdapter(adapter);
         list.setMode(PullToRefreshBase.Mode.BOTH);
+        list.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+            @Override
+            public void onLastItemVisible() {
+                Log.d("PullToRefresh","LAST ITEM VIEWED!");
+            }
+        });
         list.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2() {
             @Override
             public void onPullDownToRefresh(PullToRefreshBase pullToRefreshBase) {
+                //重新加载页面数据，从第一页开始
+                Log.d("PullToRefresh", "Refresh complete!");
                 page = 1;
                 adapter.clear();
                 adapter.addAll(Article.find(Article.class, null, null, null, null, "10"));
-                adapter.notifyDataSetChanged(); //tell table data is updated
-                Log.d("PullToRefresh", "Refresh complete!");
-                handler.postDelayed(hideRefresh, 1000);
+                handler.post(updateRunner);
             }
 
             @Override
             public void onPullUpToRefresh(PullToRefreshBase pullToRefreshBase) {
-                adapter.addAll(Article.find(Article.class, null, null, null, null, (page++ * 10) + ",10"));
-                adapter.notifyDataSetChanged(); //tell table data is updated
                 Log.d("PullToRefresh", "Load complete!");
-                handler.postDelayed(hideRefresh, 1000);
+                //加载当前最后一页开始的后10条数据
+                adapter.addAll(Article.find(Article.class, null, null, null, null, (page++ * 10) + ",10"));
+                handler.post(updateRunner);
             }
         });
     }
 
     /**
-     * This runnable will help us to terminate the refreshing label
+     * 更新UI
      */
-    private Runnable hideRefresh = new Runnable() {
+    private Runnable updateRunner = new Runnable() {
 
         @Override
         public void run() {
             list.onRefreshComplete();
-            if(list.isRefreshing()){
-                handler.postDelayed(this,1000); //check every 1s if still refreshing
-            }
+            updateMode();
+            adapter.notifyDataSetChanged(); //tell table data is updated
         }
     };
+
+    /**
+     * 更新列表刷新模式.
+     */
+    private void updateMode(){
+        long count = Article.count(Article.class);
+        if(adapter.getCount() >= count) {
+            list.setMode(PullToRefreshBase.Mode.PULL_FROM_START);   //已经没有更多记录，只能下拉刷新
+        }else {
+            list.setMode(PullToRefreshBase.Mode.BOTH);   //重新启用下拉加载更多功能
+        }
+    }
+
 }
