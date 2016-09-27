91d7dc9886ebf9182d133319ee9cd412f2a5c7ba
jgnan1981@163.com <jgnan1981@163.com>
2016-09-27 12:25:17 +0800

    Add include layout example

---
+++ android/AndroidTutorial/app/src/main/java/org/shenit/tutorial/android/fragment/IncludeExampleActivity.java
@@ -0,0 +1,15 @@
+package org.shenit.tutorial.android.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.shenit.tutorial.android.R;
+
+public class IncludeExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_include_example);
+    }
+}
